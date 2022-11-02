42e6b65e249a74f1b440bcd7f794f4f436fc32ec
Olarotimi Adamson <olarotimiadamson@yahoo.com>
2022-11-02 13:59:16 +0100

    Counter updated

--- src/pages/Counter.js
+++ src/pages/Counter.js
@@ -3,10 +3,16 @@ export default function Counter({ state, dispatch }) {
     <div className="counter--container">
       <h1>Counter</h1>
       <p>Count: {state.count}</p>
-      <input onChange={(e) => dispatch({ type: "set", valued: e })} />
+      <input type="number" value={state.count} onChange={(e) => dispatch({ type: "set", valued: e })} />
+      <button onClick={() => dispatch({ type: "incrementBy", payload: 10 })}>
+        Increment by 10
+      </button>
       <button onClick={() => dispatch({ type: "increment" })}>Increment</button>
       <button onClick={() => dispatch({ type: "reset" })}>Reset</button>
       <button onClick={() => dispatch({ type: "decrement" })}>Decrement</button>
+      <button onClick={() => dispatch({ type: "decrementBy", payload: 10 })}>
+        Decrement by 10
+      </button>
     </div>
   );
 }
