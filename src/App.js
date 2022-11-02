655a06fe6eacdc9d3f9c2b28698fd995b98e360c
Olarotimi Adamson <olarotimiadamson@yahoo.com>
2022-11-02 01:48:24 +0100

    Counter Created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer } from "react";
 
-function App() {
+export default function App() {
+  const [state, dispatch] = useReducer(
+    (state, action) => {
+      switch (action.type) {
+        case "increment":
+          return { count: state.count + 1 };
+        case "decrement":
+          return { count: state.count - 1 };
+        case "reset":
+          return { count: 0 };
+        case "set":
+          return { count: action.payload };
+        case "incrementBy":
+          return { count: state.count + action.payload };
+        case "decrementBy":
+          return { count: state.count - action.payload };
+        default:
+          return state;
+      }
+    },
+    { count: 0 }
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Count: {state.count}</h1>
+      <button onClick={() => dispatch({ type: "decrementBy", payload: 10 })}>
+        Decrement by 10
+      </button>
+      <button onClick={() => dispatch({ type: "decrement" })}>Decrement</button>
+      <button onClick={() => dispatch({ type: "reset" })}>Reset</button>
+      <button onClick={() => dispatch({ type: "set", payload: 10 })}>
+        Set Value to 10
+      </button>
+      <button onClick={() => dispatch({ type: "increment" })}>Increment</button>
+      <button onClick={() => dispatch({ type: "incrementBy", payload: 10 })}>
+        Increment by 10
+      </button>
     </div>
   );
 }
-
-export default App;
